80879ff026b32fbbebb832b837c9a0ee376d708a
ee48002 <MBelotti.external4@unicredit.eu>
2018-06-08 11:32:11 +0200

    Project setup

---
+++ src/main/java/com/cacao77/CdiMain.java
@@ -0,0 +1,14 @@
+package com.cacao77;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CdiMain {
+
+    static Logger logger = LogManager.getLogger(CdiMain.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting ");
+    }
+}
